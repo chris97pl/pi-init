7b963c162269a87e6db085309106bd579d25156b
Chris Cynarski <chris97pl@users.noreply.github.com>
2021-03-07 17:35:06 +0000

    Home Assistant script created

---
+++ homeassistant.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo docker run --init -d \
+  --name homeassistant \
+  --restart=unless-stopped \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /PATH_TO_YOUR_CONFIG:/config \
+  --network=host \
+  homeassistant/raspberrypi4-homeassistant:stable
