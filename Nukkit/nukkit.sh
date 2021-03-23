1430703fe1db13effe0e1b587a6ebbac98c8f839
Chris Cynarski <chris@cynarski.net>
2021-03-23 22:48:57 +0000

    Tweaking Dockerfile

--- Nukkit/nukkit.sh
+++ Nukkit/nukkit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo docker run -it --restart=unless-stopped -p 19132:19132 nukkit
+sudo docker run -it \
+  --restart=unless-stopped \
+  --name homeassistant \
+   -p 19132:19132 nukkit
--- Nukkit/nukkit_build.sh
+++ Nukkit/nukkit_build.sh
@@ -6,5 +6,5 @@ cd ~/nukkit
 git submodule update --init
 chmod +x mvnw
 ./mvnw clean package
-cp ~/pi-init/Nukkit/Dockerfile ./Dockerfile.pi
-sudo docker build -t nukkit - < Dockerfile.pi
+cp ~/pi-init/Nukkit/Dockerfile ./Dockerfile
+sudo docker build -t nukkit .
