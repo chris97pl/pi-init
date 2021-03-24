87517d11fece021ca7d7d961b7838a4c0ccb4a17
Chris Cynarski <chris@cynarski.net>
2021-03-24 01:36:40 +0000

    Update to pocketmine

--- pocketmine.sh
+++ pocketmine.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# sudo apt -y install autoconf automake m4 bison cmake pkg-config re2c libtool libtool-bin
-# curl -sL https://get.pmmp.io | bash -s -
-
-sudo docker run -d --name pocketmine -p 19132:19132/udp georgezero/rpi-pocketmine
+sudo apt -y install autoconf automake m4 bison cmake pkg-config re2c libtool libtool-bin screen
+mkdir ~/pocketmine
+cd ~/pocketmine
+screen -d -m -S pocketmineinstaller 'curl -sL https://get.pmmp.io | bash -s -'
+screen -d -m -S pocketmine './start.sh'
